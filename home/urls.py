51e264fd7cf3910cf91799344d03e8b5111d6e28
bek <quramboyev17@gmail.com>
2025-06-02 20:39:30 +0500

    added movie detail

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, movie_about, movie_sessions
 
 urlpatterns = [
     path('home/', home, name='home'),
+    path('movie_about/', movie_about, name='movie_about'),
+    path('movie_sessions', movie_sessions, name='movie_sessions')
 ]
--- home/views.py
+++ home/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render, redirect
 
 def home(request):
     return render(request, 'home.html')
+
+def movie_about(request):
+    return render(request, 'movie_about.html')
+
+def movie_sessions(request):
+    return render(request, 'movie_sessions.html')
