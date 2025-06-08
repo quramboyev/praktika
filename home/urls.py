8dd3a327fcd70edf6f800aa633094a37e1758f50
Asadbek <ahikmatullayev024@gmail.com>
2025-06-08 19:29:05 +0500

    [fix] home.urls

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,7 @@ from .views import home, movie_about, movie_sessions, cinema_seats
 
 urlpatterns = [
     path('', home, name='home'),
-    path('movie_about/', movie_about, name='movie_about'),
-    path('movie_sessions', movie_sessions, name='movie_sessions'),
-    path('seets/', cinema_seats, name='seets'),
+    path('movie_about/<int:pk>/', movie_about, name='movie_about'),
+    path('movie_sessions/<int:pk>/', movie_sessions, name='movie_sessions'),
+    path('seets/<int:pk>/', cinema_seats, name='seats'),
 ]
